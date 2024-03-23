eb542d7eed24ca2176419bb2461681ebc00a5417
Bacsur Viktória <bacsurv@gmail.com>
2024-03-23 19:05:23 +0100

    edited some responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,26 @@ module.exports = {
       'ubuntu': ['Ubuntu', 'sans-serif'],
       'el-messiri': ['El Messiri', 'sans-serif'],
     },
+    screens: {
+      'xs': '330px',
+      's': '420px',
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+
+    },
     extend: {},
 
   },
